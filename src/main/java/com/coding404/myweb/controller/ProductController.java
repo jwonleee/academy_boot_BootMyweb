86d953aae86e1d79f8db166cdea6e1ac839a5fba
jiwon <leejw0199@naver.com>
2023-02-14 18:31:06 +0900

    20230214 18:31 [ADD] Myweb 실습 - 판매자 페이지 1. 기본 setting 2. 템플릿 쪼개기 3. 경로 설정

---
+++ src/main/java/com/coding404/myweb/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.coding404.myweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+	//화면은 getMapping ?
+	
+	@GetMapping("/productReg")
+	public String reg() {
+		return "/product/productReg";
+	}
+	
+	@GetMapping("/productList")
+	public String list() {
+		return "/product/productList";
+	}
+	
+	@GetMapping("/productDetail")
+	public String detail() {
+		return "/product/productDetail";
+	}
+}
